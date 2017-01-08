c87c28b2645ab08e5e2145f03d4397d8b5e354f3
ko <Frdric Hamidi>
2017-01-08 19:46:49 +0100

    Basic prototype (a white window).

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,43 @@
-//! Nature of code - Following the book... in Rust!
+//! Nature of code - Following the book... in Rust, with Piston!
 //! http://natureofcode.com/
 //!
-//! Introduction - Random Walker.
+//! Introduction - Random walker.
 //!
 
-fn main() {}
+extern crate piston_app;
+
+use piston_app::*;
+
+struct App;
+
+impl App {
+    pub fn new() -> Self {
+        App {}
+    }
+}
+
+impl PistonApp for App {
+    fn setup<W, E>(&mut self, window: &mut PistonWindow<W>, e: &E, args: &RenderArgs)
+        where E: GenericEvent,
+              W: OpenGLWindow,
+              W::Event: GenericEvent
+    {
+        window.draw_2d(e, |_, gl| {
+            clear([1.0, 1.0, 1.0, 1.0], gl);
+        });
+    }
+
+    fn draw<W, E>(&mut self, window: &mut PistonWindow<W>, e: &E, args: &RenderArgs)
+        where E: GenericEvent,
+              W: OpenGLWindow,
+              W::Event: GenericEvent
+    {
+        window.draw_2d(e, |context, gl| {
+        });
+    }
+}
+
+fn main() {
+    let mut app = App::new();
+    App::run(env!("CARGO_PKG_NAME"), &mut app);
+}
