d6118302ec83b975e443fe606a4869d173d36acb
ko <Frdric Hamidi>
2017-01-08 19:10:12 +0100

    Initial commit.

---
+++ src/lib.rs
@@ -0,0 +1,37 @@
+//! Nature of code - Following the book... in Rust, with Piston!
+//! http://natureofcode.com/
+//!
+//! Simple application framework, similar to the Processing environment
+//! used in the book.
+
+extern crate piston_window;
+extern crate sdl2_window;
+
+pub use piston_window::*;
+
+use sdl2_window::Sdl2Window;
+
+pub trait PistonApp {
+    fn setup<E: GenericEvent>(&mut self, e: &E, args: &RenderArgs);
+
+    fn draw<E: GenericEvent>(&mut self, e: &E, args: &RenderArgs);
+
+    fn run<T: Into<String>>(title: T, app: &mut Self) {
+        let mut window: PistonWindow<Sdl2Window> = WindowSettings::new(title, [640, 480])
+            .exit_on_esc(true)
+            .resizable(false)
+            .vsync(true)
+            .build()
+            .unwrap();
+        let mut first = true;
+        while let Some(e) = window.next() {
+            if let Some(args) = e.render_args() {
+                if first {
+                    first = false;
+                    app.setup(&e, &args);
+                }
+                app.draw(&e, &args);
+            }
+        }
+    }
+}
