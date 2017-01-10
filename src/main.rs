5147c1516b1092004d67a845ea1129cf8da22fde
ko <Frdric Hamidi>
2017-01-10 20:44:45 +0100

    Initial commit.

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+//! Nature of code - Following the book... in Rust, with Piston!
+//! http://natureofcode.com/
+//!
+//! Introduction - Gaussian distribution graph.
+
+extern crate piston_app;
+extern crate rand;
+
+use piston_app::*;
+use rand::distributions::normal::StandardNormal;
+
+#[derive(Debug)]
+struct App;
+
+impl App {
+    fn new() -> Self {
+        App {}
+    }
+}
+
+impl PistonApp for App {
+    fn setup(&mut self, _: Context, gl: &mut G2d, _: &PistonAppState, _: &RenderArgs) {
+        clear([1.0; 4], gl);
+    }
+
+    fn draw(&mut self,
+            context: Context,
+            gl: &mut G2d,
+            _: &PistonAppState,
+            args: &RenderArgs) {
+        let mean = 320.0;
+        let sd = 60.0;
+        let StandardNormal(x) = rand::random();
+        let result = x * sd + mean;
+        ellipse([0.0, 0.0, 0.0, 0.1],
+                ellipse::circle(result, args.height as Scalar / 2.0, 16.0),
+                context.transform,
+                gl);
+    }
+}
+
+fn main() {
+    let mut app = App::new();
+    App::run(env!("CARGO_PKG_NAME"), &mut app);
+}
