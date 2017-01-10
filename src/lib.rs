2bf8434e16b9dd461d8fd3331e5d6b4552fb5ac2
ko <Frdric Hamidi>
2017-01-10 19:48:06 +0100

    Added mouse state.

--- src/lib.rs
+++ src/lib.rs
@@ -15,10 +15,54 @@ pub use types::{Color, ColorComponent};
 
 use sdl2_window::Sdl2Window;
 
-pub trait PistonApp {
-    fn setup(&mut self, _context: Context, _gl: &mut G2d, _args: &RenderArgs) {}
+#[derive(Debug)]
+pub struct PistonAppState {
+    mouse_button: MouseButton,
+    mouse_pressed: u8,
+    mouse_x: f64,
+    mouse_y: f64,
+}
 
-    fn draw(&mut self, context: Context, gl: &mut G2d, args: &RenderArgs);
+impl PistonAppState {
+    fn new() -> Self {
+        PistonAppState {
+            mouse_button: MouseButton::Unknown,
+            mouse_pressed: 0,
+            mouse_x: 0.0,
+            mouse_y: 0.0,
+        }
+    }
+
+    pub fn mouse_button(&self) -> MouseButton {
+        self.mouse_button
+    }
+
+    pub fn mouse_pressed(&self) -> bool {
+        self.mouse_pressed > 0
+    }
+
+    pub fn mouse_x(&self) -> f64 {
+        self.mouse_x
+    }
+
+    pub fn mouse_y(&self) -> f64 {
+        self.mouse_y
+    }
+}
+
+pub trait PistonApp {
+    fn setup(&mut self,
+             _context: Context,
+             _gl: &mut G2d,
+             _state: &PistonAppState,
+             _args: &RenderArgs) {
+    }
+
+    fn draw(&mut self,
+            context: Context,
+            gl: &mut G2d,
+            state: &PistonAppState,
+            args: &RenderArgs);
 
     fn run<T: Into<String>>(title: T, app: &mut Self) {
         let mut window: PistonWindow<Sdl2Window> = WindowSettings::new(title, [640, 480])
@@ -27,16 +71,30 @@ pub trait PistonApp {
             .vsync(true)
             .build()
             .unwrap();
+        let mut state = PistonAppState::new();
         let mut first = true;
         while let Some(e) = window.next() {
             if let Some(args) = e.render_args() {
                 window.draw_2d(&e, |context, gl| {
                     if first {
                         first = false;
-                        app.setup(context, gl, &args);
+                        app.setup(context, gl, &state, &args);
                     }
-                    app.draw(context, gl, &args);
+                    app.draw(context, gl, &state, &args);
                 });
+            }
+            let state = &mut state;
+            if let Some(Button::Mouse(_)) = e.press_args() {
+                state.mouse_pressed += 1;
+            }
+            if let Some(Button::Mouse(_)) = e.release_args() {
+                if state.mouse_pressed > 0 {
+                    state.mouse_pressed -= 1;
+                }
+            }
+            if let Some(position) = e.mouse_cursor_args() {
+                state.mouse_x = position[0];
+                state.mouse_y = position[1];
             }
         }
     }
