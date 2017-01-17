29ad92ef737b7099692633c57fc5bc6ed9f9c116
ko <Frdric Hamidi>
2017-01-17 21:23:26 +0100

    Initial commit.

---
+++ src/main.rs
@@ -0,0 +1,95 @@
+//! Nature of code - Following the book... in Rust, with Piston!
+//! http://natureofcode.com/
+//!
+//! Vectors - Motion 101 (constant acceleration).
+
+extern crate piston_app;
+
+use piston_app::*;
+
+#[derive(Debug)]
+struct Mover {
+    location: Vec2d,
+    velocity: Vec2d,
+    acceleration: Vec2d,
+}
+
+impl Mover {
+    fn new(state: &PistonAppState) -> Self {
+        Mover {
+            location: [state.width() / 2.0, state.height() / 2.0],
+            velocity: [0.0, 0.0],
+            acceleration: [-1e-3, 0.01],
+        }
+    }
+
+    fn draw(&self, context: Context, gfx: &mut G2d) {
+        Ellipse::new_border(color::BLACK, 1.0)
+            .color([0.5, 0.5, 0.5, 1.0])
+            .draw(ellipse::circle(self.location[0], self.location[1], 32.0),
+                  &context.draw_state,
+                  context.transform,
+                  gfx);
+    }
+
+    fn update(&mut self, state: &PistonAppState) {
+        const MAX_VELOCITY: Scalar = 10.0;
+        self.velocity = vec2_limit(vec2_add(self.velocity, self.acceleration),
+                                   MAX_VELOCITY);
+        self.location = vec2_add(self.location, self.velocity);
+        self.check_edges(state);
+    }
+
+    fn check_edges(&mut self, state: &PistonAppState) {
+        let (x, y) = (self.location[0], self.location[1]);
+        let (width, height) = (state.width(), state.height());
+        if x > width {
+            self.location[0] = 0.0;
+        } else if x < 0.0 {
+            self.location[0] = width;
+        }
+        if y > height {
+            self.location[1] = 0.0;
+        } else if y < 0.0 {
+            self.location[1] = height;
+        }
+    }
+}
+
+#[derive(Debug)]
+struct App {
+    mover: Option<Mover>,
+}
+
+impl App {
+    fn new() -> Self {
+        App { mover: None }
+    }
+
+    fn mover(&self) -> &Mover {
+        self.mover.as_ref().unwrap()
+    }
+
+    fn mover_mut(&mut self) -> &mut Mover {
+        self.mover.as_mut().unwrap()
+    }
+}
+
+impl PistonApp for App {
+    fn setup(&mut self, _: &mut PistonAppWindow, state: &PistonAppState) {
+        self.mover = Some(Mover::new(state));
+    }
+
+    fn draw(&mut self, window: &mut PistonAppWindow, state: &PistonAppState) {
+        self.mover_mut().update(state);
+        window.draw_2d(state.event(), |context, gfx| {
+            clear(color::WHITE, gfx);
+            self.mover().draw(context, gfx);
+        });
+    }
+}
+
+fn main() {
+    let mut app = App::new();
+    App::run(env!("CARGO_PKG_NAME"), &mut app);
+}
