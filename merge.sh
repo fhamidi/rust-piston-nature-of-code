5299047102182788b1aabec219c13a0438a2936d
ko <Frdric Hamidi>
2017-01-19 19:54:29 +0100

    Handling '0.Introduction'...

--- merge.sh
+++ merge.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-git remote add -f $1 ../staging/nature-of-code/$1/.git
+git remote add -f $1 ../staging/nature-of-code/0.Introduction/$1/.git
 git merge --allow-unrelated-histories $1/master
-mkdir $1
-git mv .gitignore Cargo.* rustfmt.toml src $1
+mkdir -p 0.Introduction/$1
+git mv .gitignore Cargo.* rustfmt.toml src 0.Introduction/$1
 git add .
 git commit -m "Moved '$1' into place."
 git remote remove $1
