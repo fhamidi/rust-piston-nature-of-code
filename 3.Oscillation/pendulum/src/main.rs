20b4e4a7fcc933db6d3f8f6eb6f6f6573dfbd39d
ko <Frdric Hamidi>
2018-07-29 22:58:44 +0200

    Improved pendulum can now be dragged with the mouse and draws an anchor.

--- 3.Oscillation/pendulum/src/main.rs
+++ 3.Oscillation/pendulum/src/main.rs
@@ -9,55 +9,96 @@ use piston_app::*;
 
 #[derive(Debug)]
 struct Pendulum {
-    color: Color,
-    origin: Vec2d,
-    arm_length: Scalar,
+    anchor_color: Color,
+    anchor_position: Vec2d,
+    bob_color: Color,
+    bob_position: Vec2d,
+    bob_radius: Scalar,
+    length: Scalar,
     angle: Scalar,
     angular_velocity: Scalar,
     angular_acceleration: Scalar,
     damping: Scalar,
+    dragging: bool,
 }
 
 impl Pendulum {
     fn new() -> Self {
         Pendulum {
-            color: color::TRANSPARENT,
-            origin: [0.0, 0.0],
-            arm_length: 0.0,
+            anchor_color: color::TRANSPARENT,
+            anchor_position: [0.0, 0.0],
+            bob_color: color::TRANSPARENT,
+            bob_position: [0.0, 0.0],
+            bob_radius: 32.0,
+            length: 0.0,
             angle: consts::FRAC_PI_4,
             angular_velocity: 0.0,
             angular_acceleration: 0.0,
-            damping: 0.995,
+            damping: 0.996,
+            dragging: false,
         }
     }
 
     fn setup(&mut self, state: &PistonAppState) {
-        self.color = state.random_color(Some(1.0));
-        self.origin = [state.width() / 2.0, 0.0];
-        self.arm_length = state.height() * 0.8;
+        self.anchor_color = state.random_color(Some(1.0));
+        self.bob_color = state.random_color(Some(1.0));
+        self.anchor_position = [state.width() / 2.0, state.height() / 24.0];
+        self.length = state.height() * 0.84;
     }
 
     fn draw(&self, context: Context, gfx: &mut G2d) {
-        let x = self.angle.sin() * self.arm_length + self.origin[0];
-        let y = self.angle.cos() * self.arm_length + self.origin[1];
-        Line::new(color::BLACK, 1.0).draw([self.origin[0], self.origin[1], x, y],
-                                          &context.draw_state,
-                                          context.transform,
-                                          gfx);
+        let (x, y) = (self.bob_position[0], self.bob_position[1]);
+        Line::new(color::BLACK, 1.0)
+            .draw([self.anchor_position[0], self.anchor_position[1], x, y],
+                  &context.draw_state,
+                  context.transform,
+                  gfx);
+        Rectangle::new_border(color::BLACK, 1.0)
+            .color(self.anchor_color)
+            .draw(rectangle::centered_square(self.anchor_position[0],
+                                             self.anchor_position[1],
+                                             8.0),
+                  &context.draw_state,
+                  context.transform,
+                  gfx);
         Ellipse::new_border(color::BLACK, 1.0)
-            .color(self.color)
-            .draw(ellipse::circle(x, y, 24.0),
+            .color(if self.dragging {
+                       color::BLACK
+                   } else {
+                       self.bob_color
+                   })
+            .draw(ellipse::circle(x, y, self.bob_radius),
                   &context.draw_state,
                   context.transform,
                   gfx);
     }
 
-    fn update(&mut self) {
+    fn update(&mut self, state: &PistonAppState) {
         const GRAVITY: Scalar = 0.42;
-        self.angular_acceleration = -GRAVITY / self.arm_length * self.angle.sin();
-        self.angular_velocity += self.angular_acceleration;
-        self.angle += self.angular_velocity;
-        self.angular_velocity *= self.damping;
+        if self.dragging {
+            if !state.mouse_pressed() {
+                self.angular_velocity = 0.0;
+                self.dragging = false;
+            }
+        } else if state.mouse_pressed() {
+            let distance = vec2_len(vec2_sub([state.mouse_x(), state.mouse_y()],
+                                             self.bob_position));
+            if distance < self.bob_radius {
+                self.dragging = true;
+            }
+        }
+        if self.dragging {
+            let direction = vec2_sub([state.mouse_x(), state.mouse_y()],
+                                     self.anchor_position);
+            self.angle = -vec2_heading(direction) + consts::FRAC_PI_2;
+        } else {
+            self.angular_acceleration = -GRAVITY / self.length * self.angle.sin();
+            self.angular_velocity += self.angular_acceleration;
+            self.angle += self.angular_velocity;
+            self.angular_velocity *= self.damping;
+        }
+        self.bob_position = [self.angle.sin() * self.length + self.anchor_position[0],
+                             self.angle.cos() * self.length + self.anchor_position[1]];
     }
 }
 
@@ -78,7 +119,7 @@ impl PistonApp for App {
     }
 
     fn draw(&mut self, window: &mut PistonAppWindow, state: &PistonAppState) {
-        self.pendulum.update();
+        self.pendulum.update(state);
         window.draw_2d(state.event(), |context, gfx| {
             clear(color::WHITE, gfx);
             self.pendulum.draw(context, gfx);
