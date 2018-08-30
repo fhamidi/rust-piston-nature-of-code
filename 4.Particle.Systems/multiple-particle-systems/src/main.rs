9920b4f6e247cc705f0e0c478d62dba2b7371b29
ko <frederic.hamidi@gmail.com>
2018-08-30 21:52:14 +0200

    Improved performance of particle systems. All particles are now drawn in a single pass through direct rendering with simple shaders.

--- 4.Particle.Systems/multiple-particle-systems/src/main.rs
+++ 4.Particle.Systems/multiple-particle-systems/src/main.rs
@@ -3,9 +3,31 @@
 //!
 //! Particle systems - Multiple particle systems.
 
+#[macro_use]
+extern crate gfx;
+extern crate gfx_device_gl;
 extern crate piston_app;
 
+use gfx::preset;
+use gfx::state::ColorMask;
+use gfx::traits::FactoryExt;
 use piston_app::*;
+
+gfx_defines! {
+    vertex Vertex {
+        pos: [f32; 2] = "pos",
+        uv: [f32; 2] = "uv",
+        color: [f32; 4] = "color",
+    }
+
+    pipeline particles {
+        vbuf: gfx::VertexBuffer<Vertex> = (),
+        texture: gfx::TextureSampler<[f32; 4]> = "tex",
+        out: gfx::BlendTarget<gfx::format::Srgba8> = ("o_Color",
+                                                      ColorMask::all(),
+                                                      preset::blend::ALPHA),
+    }
+}
 
 #[derive(Debug)]
 struct Particle {
@@ -33,21 +55,52 @@ impl Particle {
         self.life > 0.0
     }
 
-    fn draw(&self,
-            texture: &G2dTexture,
-            state: &PistonAppState,
-            context: Context,
-            gfx: &mut G2d) {
-        state.draw_centered_texture(texture,
-                                    Some([self.color[0],
-                                          self.color[1],
-                                          self.color[2],
-                                          self.life as ColorComponent]),
-                                    self.position[0],
-                                    self.position[1],
-                                    &context.draw_state,
-                                    context.transform,
-                                    gfx);
+    #[inline]
+    fn normalize_x(&self, state: &PistonAppState, x: Scalar) -> f32 {
+        state.map_range(x, 0.0, state.width(), -1.0, 1.0) as f32
+    }
+
+    #[inline]
+    fn normalize_y(&self, state: &PistonAppState, y: Scalar) -> f32 {
+        -state.map_range(y, 0.0, state.height(), -1.0, 1.0) as f32
+    }
+
+    fn extend_vertex_buffer(&self,
+                            state: &PistonAppState,
+                            vertices: &mut Vec<Vertex>,
+                            indices: &mut Vec<u32>) {
+        const HALF_SIDE: Scalar = 8.0;
+        let start = vertices.len() as u32;
+        let color = [self.color[0],
+                     self.color[1],
+                     self.color[2],
+                     self.life as ColorComponent];
+        let (x, y) = (self.position[0], self.position[1]);
+        vertices.extend(&[Vertex {
+                              pos: [self.normalize_x(state, x + HALF_SIDE),
+                                    self.normalize_y(state, y + HALF_SIDE)],
+                              uv: [1.0, 1.0],
+                              color: color,
+                          },
+                          Vertex {
+                              pos: [self.normalize_x(state, x - HALF_SIDE),
+                                    self.normalize_y(state, y + HALF_SIDE)],
+                              uv: [0.0, 1.0],
+                              color: color,
+                          },
+                          Vertex {
+                              pos: [self.normalize_x(state, x - HALF_SIDE),
+                                    self.normalize_y(state, y - HALF_SIDE)],
+                              uv: [0.0, 0.0],
+                              color: color,
+                          },
+                          Vertex {
+                              pos: [self.normalize_x(state, x + HALF_SIDE),
+                                    self.normalize_y(state, y - HALF_SIDE)],
+                              uv: [1.0, 0.0],
+                              color: color,
+                          }]);
+        indices.extend(&[start, start + 1, start + 2, start + 2, start + 3, start]);
     }
 
     fn update(&mut self) {
@@ -76,19 +129,18 @@ impl ParticleSystem {
         }
     }
 
-    fn draw(&self,
-            texture: &G2dTexture,
-            state: &PistonAppState,
-            context: Context,
-            gfx: &mut G2d) {
-        for particle in &self.particles {
-            particle.draw(texture, state, context, gfx);
-        }
+    #[inline]
+    fn len(&self) -> usize {
+        self.particles.len()
     }
 
-    #[inline]
-    fn particle_count(&self) -> usize {
-        self.particles.len()
+    fn extend_vertex_buffer(&self,
+                            state: &PistonAppState,
+                            vertices: &mut Vec<Vertex>,
+                            indices: &mut Vec<u32>) {
+        for particle in &self.particles {
+            particle.extend_vertex_buffer(state, vertices, indices);
+        }
     }
 
     fn spawn_particle(&mut self, state: &PistonAppState) {
@@ -112,19 +164,31 @@ impl ParticleSystem {
 #[derive(Debug)]
 struct App {
     particle_texture: Option<G2dTexture>,
+    pipeline: Option<gfx::pso::PipelineState<gfx_device_gl::Resources, particles::Meta>>,
     particle_systems: Vec<ParticleSystem>,
+    vertices: Vec<Vertex>,
+    indices: Vec<u32>,
 }
 
 impl App {
     fn new() -> Self {
         App {
             particle_texture: None,
+            pipeline: None,
             particle_systems: vec![],
+            vertices: Vec::with_capacity(4 * 4096),
+            indices: Vec::with_capacity(6 * 4096),
         }
     }
 
     fn particle_texture(&self) -> &G2dTexture {
         self.particle_texture.as_ref().unwrap()
+    }
+
+    fn pipeline
+        (&self)
+         -> &gfx::pso::PipelineState<gfx_device_gl::Resources, particles::Meta> {
+        self.pipeline.as_ref().unwrap()
     }
 
     fn spawn_particle_system(&mut self, state: &PistonAppState) {
@@ -135,14 +199,21 @@ impl App {
 
 impl PistonApp for App {
     fn setup(&mut self, window: &mut PistonAppWindow, state: &PistonAppState) {
-        const MAX_INITIAL_SYSTEMS: usize = 3;
-        self.particle_texture = Some(Texture::from_path(&mut window.factory,
+        const MAX_INITIAL_PARTICLE_SYSTEMS: usize = 9;
+        let factory = &mut window.factory;
+        self.particle_texture = Some(Texture::from_path(factory,
                                                         "assets/particle.png",
                                                         Flip::None,
                                                         &TextureSettings::new())
                                          .unwrap());
+        self.pipeline =
+            Some(factory
+                     .create_pipeline_simple(include_bytes!("particle_150_core.glslv"),
+                                             include_bytes!("particle_150_core.glslf"),
+                                             particles::new())
+                     .unwrap());
         let mut rng = thread_rng();
-        self.particle_systems = (0..MAX_INITIAL_SYSTEMS)
+        self.particle_systems = (0..MAX_INITIAL_PARTICLE_SYSTEMS)
             .map(|_| {
                      ParticleSystem::new(rng.gen_range(42.0, state.width() - 42.0),
                                          rng.gen_range(42.0, state.height() - 42.0))
@@ -152,26 +223,40 @@ impl PistonApp for App {
 
     fn draw(&mut self, window: &mut PistonAppWindow, state: &PistonAppState) {
         if state.key_hit(Key::D) {
+            let total_particle_count: usize = self.particle_systems
+                .iter()
+                .map(|particle_system| particle_system.len())
+                .sum();
             println!("Frame {} | Particle systems: {} | Total particles: {}",
                      state.frame_count(),
                      self.particle_systems.len(),
-                     self.particle_systems
-                         .iter()
-                         .map(|ps| ps.particle_count())
-                         .sum::<usize>());
+                     total_particle_count);
         }
         if state.mouse_button_clicked(MouseButton::Left) {
             self.spawn_particle_system(state);
         }
+        self.vertices.clear();
+        self.indices.clear();
         for particle_system in &mut self.particle_systems {
             particle_system.update(state);
+            particle_system
+                .extend_vertex_buffer(state, &mut self.vertices, &mut self.indices);
         }
-        window.draw_2d(state.event(), |context, gfx| {
-            clear(color::WHITE, gfx);
-            for particle_system in &self.particle_systems {
-                particle_system.draw(self.particle_texture(), state, context, gfx);
-            }
-        });
+        let (vbuf, slice) =
+            window
+                .factory
+                .create_vertex_buffer_with_slice(&self.vertices[..], &self.indices[..]);
+        let texture = self.particle_texture();
+        let encoder = &mut window.encoder;
+        encoder.clear(&window.output_color, color::WHITE);
+        encoder.draw(&slice,
+                     self.pipeline(),
+                     &particles::Data {
+                         vbuf: vbuf,
+                         texture: (texture.view.clone(), texture.sampler.clone()),
+                         out: window.output_color.clone(),
+                     });
+        encoder.flush(&mut window.device);
     }
 }
 
